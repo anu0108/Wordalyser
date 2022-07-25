6983c5f9c56c8551415211335e3ece1c9bf28e8c
anu0108 <108670241+anu0108@users.noreply.github.com>
2022-07-25 19:02:46 +0530

    first commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -61,7 +61,7 @@ export default function TextForm(props) {
     </div>
     <div className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>
       <h2>Your text summary</h2>
-      <p> {text.split(/[]+/).filter((a1)=>{return a1.length!==0}).length} words and {text.length} characters</p>
+      <p> {text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").filter((a1)=>{return a1.length!==0}).length} Minutes read</p>
       <h2>Preview</h2>
       <p>{text.length>0?text:"Nothing to preview"}</p>
